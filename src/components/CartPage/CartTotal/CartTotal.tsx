e02911f16d73c27a434f7ba1b56b99e94265bd05
rahulsd380 <rahulsd380@gmail.com>
2025-06-30 16:10:09 +0600

    Implemented payment method partially

--- src/components/CartPage/CartTotal/CartTotal.tsx
+++ src/components/CartPage/CartTotal/CartTotal.tsx
@@ -1,48 +1,113 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from "axios";
 import { IMAGES } from "../../../assets";
 import { TCartData } from "../../../types/cartData.types";
+import { useSelector } from "react-redux";
+import { useCurrentUser } from "../../../redux/Features/Auth/authSlice";
+import { useState } from "react";
+import { useMakePaymentMutation } from "../../../redux/Features/Payment/paymentApi";
+
+// Add Razorpay type to window
+declare global {
+  interface Window {
+    Razorpay: any;
+  }
+}
 
 const CartTotal = ({ cartData }: { cartData: TCartData[] }) => {
-    const discountedPriceTotal = cartData && cartData?.reduce((acc, currVal) => acc + currVal.discountedPrice, 0);
-    const gst = discountedPriceTotal * 18 / 100;
+  const discountedPriceTotal =
+    cartData &&
+    cartData?.reduce((acc, currVal) => acc + currVal.discountedPrice, 0);
+  const gst = (discountedPriceTotal * 18) / 100;
+  const user = useSelector(useCurrentUser) as any;
 
-    return (
-        <div className=" w-full xl:w-[500px] flex flex-col gap-6 bg-white rounded-2xl border border-neutral-75 p-5">
-            {/* Heading */}
-            <div className="flex items-center justify-between">
-                <h1 className="heading6 text-sm">Item Total</h1>
-                <div className="flex items-center gap-[6px]">
-                    <h1 className="heading6 text-sm">₹{discountedPriceTotal}</h1>
-                </div>
-            </div>
-            <div className="flex items-center justify-between">
-                <h1 className="heading6 text-sm">GST 18%</h1>
-                <div className="flex items-center gap-[6px]">
-                    <h1 className="heading6 text-sm">₹{gst}</h1>
-                </div>
-            </div>
+  const cartItems = cartData?.map((item) => item._id);
 
-            <hr className="border border-neutral-60" />
+  const [makePayment] = useMakePaymentMutation();
 
-            <div className="flex items-center justify-between">
-                <div>
-                    <h1 className="heading6">To Pay </h1>
-                </div>
-                <div>
-                    <h1 className="heading6">₹{discountedPriceTotal + gst}</h1>
-                </div>
-            </div>
+  const [loading, setLoading] = useState(false);
 
-            <div className="flex flex-col gap-4">
-                <button className="bg-primary-gradient-light px-5 py-[10px] text-primary-10 font-semibold leading-6 rounded-[10px] shadow-primary-shadow w-full">Proceed to Payment</button>
+  const totalAmount = 10;
+  const handleCheckout = async () => {
+    try {
+      setLoading(true);
+      const keyData = await axios.get("http://localhost:5000/api/v1/getKey");
 
-                <div className="flex items-center justify-center gap-4">
-                    <img src={IMAGES.securePayment} alt="secure-payment" className="" />
-                    <img src={IMAGES.razorpay} alt="razorpay" className="" />
-                    <img src={IMAGES.secureSSL} alt="secure-payment" className="" />
-                </div>
-            </div>
+      const response = await makePayment({ amount: Number(totalAmount) });
+
+      const options = {
+        key: keyData?.data?.key, // Razorpay key_id
+        amount: response?.data?.order?.amount,
+        currency: "INR",
+        name: "PMGURUKKUL",
+        description: "Test Transaction",
+        image: "https://i.ibb.co.com/SmbFbDC/footer-Logo.png",
+        order_id: response?.data?.order?.id, // the order id
+        callback_url: "http://localhost:5000/api/v1/paymentVerification",
+        prefill: {
+          name: user?.name,
+          email: user?.email,
+          userId: user?._id,
+        },
+        theme: {
+          color: "#7E77D6",
+        },
+      };
+      console.log(window);
+      const rzp = new window.Razorpay(options);
+      rzp.open();
+
+      localStorage.setItem("orderedCourses", JSON.stringify(cartItems));
+    } catch (error) {
+      console.error("Error during checkout:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className=" w-full xl:w-[500px] flex flex-col gap-6 bg-white rounded-2xl border border-neutral-75 p-5">
+      {/* Heading */}
+      <div className="flex items-center justify-between">
+        <h1 className="heading6 text-sm">Item Total</h1>
+        <div className="flex items-center gap-[6px]">
+          <h1 className="heading6 text-sm">₹{discountedPriceTotal}</h1>
         </div>
-    );
+      </div>
+      <div className="flex items-center justify-between">
+        <h1 className="heading6 text-sm">GST 18%</h1>
+        <div className="flex items-center gap-[6px]">
+          <h1 className="heading6 text-sm">₹{gst}</h1>
+        </div>
+      </div>
+
+      <hr className="border border-neutral-60" />
+
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="heading6">To Pay </h1>
+        </div>
+        <div>
+          <h1 className="heading6">₹{discountedPriceTotal + gst}</h1>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <button
+          onClick={handleCheckout}
+          className="bg-primary-gradient-light px-5 py-[10px] text-primary-10 font-semibold leading-6 rounded-[10px] shadow-primary-shadow w-full"
+        >
+          {loading ? "Loading..." : "Proceed to Payment"}
+        </button>
+
+        <div className="flex items-center justify-center gap-4">
+          <img src={IMAGES.securePayment} alt="secure-payment" className="" />
+          <img src={IMAGES.razorpay} alt="razorpay" className="" />
+          <img src={IMAGES.secureSSL} alt="secure-payment" className="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartTotal;
--- src/pages/PaymentSuccessful/PaymentSuccessful.tsx
+++ src/pages/PaymentSuccessful/PaymentSuccessful.tsx
@@ -1,18 +1,68 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Link, useParams } from "react-router-dom";
 import { FaCheckCircle } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { useCreateOrderMutation } from "../../redux/Features/Payment/paymentApi";
 
 const PaymentSuccessful = () => {
-   const { paymentId } = useParams();
-  return (
+  const { paymentId } = useParams();
+  const [orderedCourses, setOrderedCourses] = useState([]);
+  // const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [success, setSuccess] = useState(false);
+  const [createOrder] = useCreateOrderMutation();
+
+  useEffect(() => {
+    const storedCourses = localStorage.getItem("orderedCourses");
+    const courses = storedCourses ? JSON.parse(storedCourses) : [];
+    setOrderedCourses(courses);
+  }, []);
+
+  useEffect(() => {
+    const handlePushOrderedItems = async () => {
+      try {
+        const courseIds = orderedCourses?.map((data: any) => data?._id);
+        const orderInfo = {
+          courseId: courseIds,
+        };
+        await createOrder(orderInfo).unwrap();
+        setSuccess(true);
+        localStorage.removeItem("orderedCourses");
+      } catch (error) {
+        console.error("Error placing order:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (orderedCourses.length > 0) {
+      handlePushOrderedItems();
+    }
+  }, [orderedCourses, createOrder]);
+
+  if (loading) {
+    return (
+      <div className="flex flex-col items-center text-center mt-32">
+        <p className="text-neutral-10 text-lg">Processing your payment...</p>
+      </div>
+    );
+  }
+
+  return success ? (
     <div className="min-h-screen flex items-center justify-center px-4 py-10">
       <div className="bg-white shadow-lg rounded-2xl p-8 text-center max-w-md w-full">
         <div className="flex justify-center mb-4">
           <FaCheckCircle className="text-green-500 text-8xl animate-pulse" />
         </div>
-        <h2 className="text-2xl font-bold text-gray-800 mb-2">Payment Successful!</h2>
-        <h2 className="text-xl font-medium text-gray-800 mb-2">Payment id : {paymentId}</h2>
+        <h2 className="text-2xl font-bold text-gray-800 mb-2">
+          Payment Successful!
+        </h2>
+        <h2 className="text-xl font-medium text-gray-800 mb-2">
+          Payment id : {paymentId}
+        </h2>
         <p className="text-gray-600 mb-6">
-          Thank you for your purchase. Your transaction has been successfully completed.
+          Thank you for your purchase. Your transaction has been successfully
+          completed.
         </p>
         <Link
           to="/courses"
@@ -21,6 +71,12 @@ const PaymentSuccessful = () => {
           Go to Courses
         </Link>
       </div>
+    </div>
+  ) : (
+    <div className="flex flex-col items-center text-center mt-32">
+      <p className="text-red-500 text-lg">
+        Failed to process your order. Please try again.
+      </p>
     </div>
   );
 };
